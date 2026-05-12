1ae9ca90743dd1d293404fbc28b7f737424733f6
Some Guy <85842201+ced4rtree@users.noreply.github.com>
2026-05-11 22:34:49 -0600

    Parser refactor (#5)
    * [parser] simplify getter code for cli args
    
    * [parser] remove randam ahh $ from number formatter
    
    * [lib] document Parser macro
    
    * [lib] put getter generation in its own function
    
    * [parser,lib] macros ftw holy cow
    
    Need to work on error handling for the absent handlers. Used to just
    pass the error on through the ? symbol, but that doesn't work with the
    current implementation for absent handlers, so I've resorted to calling
    .unwrap() everywhere for now.
    
    * [lib] formatting & comments
    
    * clippy :D
    
    * [lib] typo
    
    * [lib] correction to documentation following new absent_handler macro

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,142 @@ extern crate syn;
 extern crate quote;
 
 use proc_macro::TokenStream;
+use quote::ToTokens;
+use regex::Regex;
+use syn::{Attribute, DataStruct, DeriveInput, Fields, FieldsNamed, Meta, Type};
+
+fn is_option(ty: &Type) -> bool {
+    let option_regex = Regex::new("Option( )?<.*>").unwrap();
+    let field_type_str = ty.to_token_stream().to_string();
+    option_regex.is_match(field_type_str.as_str())
+}
+
+/// Generate code for getting parser fields. Useful for bypassing the Option<T>
+/// automatically, which is necessary to mark an argument as optional for clap,
+/// but unnecessary within Robostart since we prompt for any missing info.
+fn parser_gen_getters(fields: &FieldsNamed) -> Vec<proc_macro2::TokenStream> {
+    fields.named.iter().map(|field| {
+        let field_name = &field.ident;
+        let mut field_type = field.ty.to_token_stream();
+
+        // remove Option<> from field_type for the getter
+        let is_option = is_option(&field.ty); 
+        if is_option {
+            let type_tokens: Vec<_> = field.ty.to_token_stream().into_iter().collect();
+            let inner_tokens = type_tokens[2..type_tokens.len()-1].to_vec();
+            field_type = proc_macro2::TokenStream::from_iter(inner_tokens);
+        }
+
+        let getter_ret: syn::Expr = match is_option {
+            true => syn::parse_quote!(self.#field_name.as_ref().unwrap()),
+            false => syn::parse_quote!(&self.#field_name)
+        };
+        quote! {
+            pub fn #field_name(&self) -> &#field_type {
+                #getter_ret
+            }
+        }
+    }).collect()
+}
+
+/// Generate all the code that will run if an optional value is not present.
+/// It is assumed that the absent handler will populate the data.
+fn parser_gen_absent_handlers(fields: &FieldsNamed) -> Vec<proc_macro2::TokenStream> {
+    fields.named.iter().filter_map(|field| {
+        let ident = &field.ident;
+        let is_option = is_option(&field.ty);
+
+        // don't need to handle a non-optional field
+        if !is_option {
+            return None;
+        }
+
+        let handler_attrs: Vec<&Attribute> = field
+            .attrs
+            .iter()
+            .filter(|field| field.path().is_ident("absent_handler"))
+            .collect();
+        let handler = match handler_attrs.len() {
+            ..=0 => panic!("All elements of type Option<T> in a struct that derives robostart::Parser must define an absent handler through the #[absent_handler(...)] attribute"),
+            1 => handler_attrs[0],
+            2.. => panic!("You may not define multiple absent handlers for one element in a struct that derives robostart::Parser"),
+        };
+
+        let handler_tokens = match &handler.meta {
+            Meta::List(lst) => &lst.tokens,
+            _ => panic!("absent_handler helper macro should be assigned in list form, e.g. #[absent_handler(...)]. Found #[{:?}] instead.", handler), 
+        };
+
+        Some(quote! {
+            if self.#ident.is_none() {
+                self.#ident = Some(#handler_tokens)
+            }
+        })
+    }).collect()
+}
+
+/// Special Robostart parser
+///
+/// Automatically generates getters for all elements in a struct, returning
+/// self.<elem>.as_ref().unwrap() if the element is of type Option<T>.
+///
+/// Designed for Robostart's use case of having all options capable of being
+/// input through CLI args while prompting the user for anything not submitted
+/// that way. This enables a single struct definition for clap arguments while
+/// reducing boilerplate around navigating the necessary Option<> handling that
+/// comes with that.
+///
+/// # Examples
+///
+/// ```
+/// #[derive(robostart::Parser, clap::Parser)]
+/// struct CliParser {
+///     #[arg(short, long)]
+///     #[absent_handler(prompt_u32())]
+///     arg1: Option<u32>
+///
+///     #[arg(short, long)]
+///     arg2: String
+/// }
+///
+/// impl CliParser {
+///     pub fn print_args(&self) {
+///         // Getters are automatically generated for arg1 and arg2.
+///         // The getters for optional values assume a value has been populated,
+///         // as Robostart will prompt the user for any absent values.
+///         println!("arg1: {}, arg2: {}", self.arg1(), self.arg2());
+///     }
+/// }
+/// ```
+#[proc_macro_derive(Parser, attributes(absent_handler))]
+pub fn parser(input: TokenStream) -> TokenStream {
+    let syn_item = syn::parse_macro_input!(input as DeriveInput);
+
+    let name = &syn_item.ident.to_token_stream();
+
+    match syn_item.data {
+        syn::Data::Struct(DataStruct {
+            fields: Fields::Named(ref fields),
+            ..
+        }) => {
+            let field_getters = parser_gen_getters(fields);
+            let absent_handlers = parser_gen_absent_handlers(fields);
+
+            quote! {
+                impl #name {
+                    #(#field_getters)*
+
+                    pub fn handle_absent_values(&mut self) {
+                        #(#absent_handlers)*
+                    }
+                }
+            }.into()
+        }
+        _ => {
+            panic!("Parser macro can only be used with structs that have named elements.");
+        }
+    }
+}
 
 #[proc_macro_derive(AllVariants)]
 pub fn derive_all_variants(input: TokenStream) -> TokenStream {
--- src/parser.rs
+++ src/parser.rs
@@ -31,113 +31,66 @@ impl Display for ProjectType {
     }    
 }
 
-#[derive(clap::Parser, Debug)]
+#[derive(robostart::Parser, clap::Parser, Debug)]
 #[command(version, about, long_about = None)]
-struct Args {
+pub struct CliParser {
     /// Language to initialize
     #[arg(short, long)]
+    #[absent_handler(Select::new(
+        "Language: ",
+        Language::all_variants().to_vec()
+    ).prompt().unwrap())]
     language: Option<Language>,
 
     /// Type of project to initalize
     #[arg(short, long)]
+    #[absent_handler(Select::new(
+        "Project Type: ",
+        ProjectType::all_variants().to_vec()
+    ).prompt().unwrap())]
     project_type: Option<ProjectType>,
 
     /// What version to download
     #[arg(short, long)]
+    #[absent_handler({
+        let proj_type = self.project_type();
+        Text::new(
+            format!("{} Version: ", proj_type).as_str()
+        ).with_help_message("This will match the corresponding WPILib version, e.g. 2025.3.2")
+        .prompt()
+        .unwrap()
+    })]
     wpilib_version: Option<String>,
 
     /// The parent directory for the new project
     #[arg(short, long)]
+    #[absent_handler(PathBuf::from(Text::new(
+        "What directory should the project live under?"
+    ).with_help_message("This is just the parent directory of your project, don't include the project name.")
+        .prompt()
+        .unwrap()
+        .replace("~", std::env::home_dir().unwrap().to_str().unwrap())))]
     output_prefix: Option<PathBuf>,
 
     /// Name of the new project
     #[arg(short, long)]
+    #[absent_handler(Text::new("Project Name: ").prompt().unwrap())]
     name: Option<String>,
 
     /// Your team number
     #[arg(short, long)]
+    #[absent_handler(CustomType::new("Team Number: ")
+        .with_formatter(&|i: u32| format!("{i}"))
+        .with_error_message("Please type a valid integer greater than 0")
+        .prompt()
+        .unwrap())]
     team_number: Option<u32>,
-}
-
-pub struct CliParser {
-    args: Args
 }
 
 impl CliParser {
     pub fn new() -> Result<Self, InquireError> {
-        let mut ret = CliParser {
-            args: Args::parse(),
-        };
-
-        if ret.args.language.is_none() {
-            ret.args.language = Some(Select::new(
-                "Language: ",
-                Language::all_variants().to_vec()
-            ).prompt()?);
-        }
-
-        if ret.args.project_type.is_none() {
-            ret.args.project_type = Some(Select::new(
-                "Project Type: ",
-                ProjectType::all_variants().to_vec()
-            ).prompt()?);
-        }
-
-        if ret.args.wpilib_version.is_none() {
-            let proj_type = ret.args
-                .project_type
-                .as_ref()
-                .unwrap();
-            ret.args.wpilib_version = Some(Text::new(
-                format!("{} Version: ", proj_type).as_str()
-            ).with_help_message("This will match the corresponding WPILib version, e.g. 2025.3.2")
-                .prompt()?);
-        }
-
-        if ret.args.output_prefix.is_none() {
-            ret.args.output_prefix = Some(PathBuf::from(Text::new(
-                "What directory should the project live under?"
-            ).with_help_message("This is just the parent directory of your project, don't include the project name.")
-                .prompt()?
-                .replace("~", std::env::home_dir().unwrap().to_str().unwrap())))
-        }
-
-        if ret.args.name.is_none() {
-            ret.args.name = Some(Text::new("Project Name: ")
-                .prompt()?);
-        }
-
-        if ret.args.team_number.is_none() {
-            ret.args.team_number = Some(CustomType::new("Team Number: ")
-                .with_formatter(&|i: u32| format!("${i}"))
-                .with_error_message("Please type a valid integer greater than 0")
-                .prompt()?)
-        }
-
+        let mut ret = CliParser::parse();
+        ret.handle_absent_values();
         Ok(ret)
-    }
-
-    pub fn language(&self) -> Language {
-        self.args.language.clone().unwrap()
-    }
-
-    pub fn project_type(&self) -> ProjectType {
-        self.args.project_type.clone().unwrap()
-    }
-
-    pub fn wpilib_version(&self) -> String {
-        self.args.wpilib_version.clone().unwrap()
-    }
-
-    pub fn output_prefix(&self) -> PathBuf {
-        self.args.output_prefix.clone().unwrap()
-    }
-
-    pub fn name(&self) -> String {
-        self.args.name.clone().unwrap()
-    }
-
-    pub fn team_number(&self) -> u32 {
-        self.args.team_number.unwrap()
     }
 }
