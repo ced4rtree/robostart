289ade09d2ef74b036e0af9ba034ec20430ce9ac
ced4rtree <usernamesarenoteasy@proton.me>
2026-05-14 23:25:09 -0600

    [parser,lib] Propagate parser errors with anyhow

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,8 @@
 /// Parse user supplied arguments
 use std::{fmt::{Display, Formatter}, path::PathBuf};
 
-use inquire::{CustomType, InquireError, Select, Text};
+use anyhow::Context;
+use inquire::{CustomType, Select, Text};
 use robostart::AllVariants;
 use clap::{Parser, ValueEnum};
 
@@ -36,61 +37,78 @@ impl Display for ProjectType {
 pub struct CliParser {
     /// Language to initialize
     #[arg(short, long)]
-    #[absent_handler(Select::new(
+    #[absent_handler(|_| Select::new(
         "Language: ",
         Language::all_variants().to_vec()
-    ).prompt().unwrap())]
+    )
+        .prompt()
+        .with_context(|| format!("Failed to prompt for language selection.")))]
     language: Option<Language>,
 
     /// Type of project to initalize
     #[arg(short, long)]
-    #[absent_handler(Select::new(
+    #[absent_handler(|_| Select::new(
         "Project Type: ",
         ProjectType::all_variants().to_vec()
-    ).prompt().unwrap())]
+    )
+        .prompt()
+        .with_context(|| format!("Failed to prompt for the project type.")))]
     project_type: Option<ProjectType>,
 
     /// What version to download
     #[arg(short, long)]
-    #[absent_handler({
-        let proj_type = self.project_type();
-        Text::new(
-            format!("{} Version: ", proj_type).as_str()
-        ).with_help_message("This will match the corresponding WPILib version, e.g. 2025.3.2")
-        .prompt()
-        .unwrap()
+    #[absent_handler(|parser: &Self| -> Result<String, anyhow::Error> {
+        let proj_type = parser.project_type();
+        Text::new(format!("{} Version: ", proj_type).as_str())
+            .with_help_message("This will match the corresponding WPILib version, e.g. 2025.3.2")
+            .prompt()
+            .with_context(|| format!("Failed to prompt for WPILib version."))
     })]
     wpilib_version: Option<String>,
 
     /// The parent directory for the new project
     #[arg(short, long)]
-    #[absent_handler(PathBuf::from(Text::new(
-        "What directory should the project live under?"
-    ).with_help_message("This is just the parent directory of your project, don't include the project name.")
-        .prompt()
-        .unwrap()
-        .replace("~", std::env::home_dir().unwrap().to_str().unwrap())))]
+    // idk why the compiler can't figure out that this returns an anyhow::Error
+    #[absent_handler(|_| -> Result<_, anyhow::Error> {
+        let prompt = Text::new("What directory should the project live under?")
+            .with_help_message("This is just the parent directory of your project, don't include the project name.")
+            .prompt()
+            .with_context(|| "Failed to prompt for output prefix.")?;
+
+        // grrr temporary value dropped while borrowed
+        let home_dir = std::env::home_dir()
+            .with_context(|| "Failed to read user\'s home directory into a `PathBuf`.")?;
+        let home_dir = home_dir
+            .to_str()
+            .with_context(|| "Failed to read home directory path into a string.")?;
+
+        let path_str = prompt.replace("~", home_dir); 
+
+        Ok(PathBuf::from(path_str))
+    })]
     output_prefix: Option<PathBuf>,
 
     /// Name of the new project
     #[arg(short, long)]
-    #[absent_handler(Text::new("Project Name: ").prompt().unwrap())]
+    #[absent_handler(|_| Text::new("Project Name: ")
+        .prompt()
+        .with_context(|| format!("Failed to prompt for project name.")))]
     name: Option<String>,
 
     /// Your team number
     #[arg(short, long)]
-    #[absent_handler(CustomType::new("Team Number: ")
+    #[absent_handler(|_| CustomType::new("Team Number: ")
         .with_formatter(&|i: u32| format!("{i}"))
         .with_error_message("Please type a valid integer greater than 0")
         .prompt()
-        .unwrap())]
+        .with_context(|| format!("Could not read team number input.")))]
     team_number: Option<u32>,
 }
 
 impl CliParser {
-    pub fn new() -> Result<Self, InquireError> {
+    pub fn new() -> Result<Self, anyhow::Error> {
         let mut ret = CliParser::parse();
-        ret.handle_absent_values();
+        ret.handle_absent_values()?;
         Ok(ret)
     }
 }
