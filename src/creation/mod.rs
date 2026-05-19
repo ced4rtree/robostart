09e2c9ef475be4af12e98be5c46d2f3953ec2a84
ced4rtree <usernamesarenoteasy@proton.me>
2026-05-19 14:15:17 -0600

    clippy

--- src/creation/mod.rs
+++ src/creation/mod.rs
@@ -5,17 +5,17 @@ mod unpack;
 
 pub async fn create_project(config: &Config) -> anyhow::Result<()> {
     // fetch selected project from github into cache
-    fetcher::fetch_project(&config).await?;
+    fetcher::fetch_project(config).await?;
 
     unpack::unpack_fetched_zip(
-        &cache::get_project_zip_path(&config)?,
-        &cache::get_project_unzipped_path(&config)?,
+        &cache::get_project_zip_path(config)?,
+        &cache::get_project_unzipped_path(config)?,
     )?;
 
     // transfer project from cache into install dir
     unpack::install_project(
-        &cache::get_project_unzipped_path(&config)?,
-        &config
+        &cache::get_project_unzipped_path(config)?,
+        config
     )?;
 
     Ok(())
